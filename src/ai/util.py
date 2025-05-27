3a2030379223f83b6a88ae6a652c4e290698b670
lokmanzeddoun <zeddounlokmane@yahoo.com>
2025-05-27 15:02:35 +0100

    feat:enhance the realtime

--- src/ai/util.py
+++ src/ai/util.py
@@ -12,12 +12,7 @@ dict_char_to_int = {'O': '0',
                     'G': '6',
                     'S': '5'}
 
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
+
 
 
 def write_csv(results, output_path):
@@ -61,6 +56,7 @@ def write_csv(results, output_path):
 def license_complies_format(text):
     """
     Check if the license plate text complies with the required format.
+    Only allows numbers 0-9 and total length must be <= 11.
 
     Args:
         text (str): License plate text.
@@ -68,41 +64,26 @@ def license_complies_format(text):
     Returns:
         bool: True if the license plate complies with the format, False otherwise.
     """
-    if len(text) != 7:
+    # Check if length is <= 11
+    if len(text) > 11:
         return False
-
-    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
-       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
-       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
-       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
-       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
-       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
-        return True
-    else:
-        return False
+        
+    # Check if all characters are digits 0-9
+    return all(char.isdigit() for char in text)
 
 
 def format_license(text):
     """
-    Format the license plate text by converting characters using the mapping dictionaries.
+    Format the license plate text. Since we only accept numbers 0-9,
+    we simply return the input text.
 
     Args:
         text (str): License plate text.
 
     Returns:
-        str: Formatted license plate text.
+        str: The input text (since we only accept numbers 0-9).
     """
-    license_plate_ = ''
-    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
-               2: dict_char_to_int, 3: dict_char_to_int}
-    for j in [0, 1, 2, 3, 4, 5, 6]:
-        if text[j] in mapping[j].keys():
-            license_plate_ += mapping[j][text[j]]
-        else:
-            license_plate_ += text[j]
-
-    return license_plate_
+    return text
 
 
 def read_license_plate(license_plate_crop):
@@ -117,7 +98,6 @@ def read_license_plate(license_plate_crop):
     """
 
     detections = reader.readtext(license_plate_crop)
-
     for detection in detections:
         bbox, text, score = detection
 
